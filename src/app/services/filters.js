0143986e80d1e02ca2428caa82029ee0da05ed43
Hom Yan <yanzhihong23@qq.com>
2015-12-05 23:27:29 +0800

    add filters

--- src/app/services/filters.js
+++ src/app/services/filters.js
@@ -3,20 +3,91 @@
 
   angular
     .module('crmApp')
-    .filter('status', status);
+    .filter('payment', payment)
+    .filter('payMode', payMode)
+    .filter('applyType', applyType)
+    .filter('applicantType', applicantType)
+    .filter('agencyType', agencyType)
+    .filter('inviteStatus', inviteStatus)
+    .filter('applyRole', applyRole);
 
   /** @ngInject */
-  function status() {
+  function applyType() {
     var map = {
-      '0': '发布中',
-      '1': '进行中',
-      '3': '待评价',
-      '4': '已完成',
-    }
+      '0': '经销权',
+      '1': '开店权'
+    };
 
     return function(id) {
       return map[id] || id;
     };
-    
+  }
+
+  function applicantType() {
+    var map = {
+      '0': '个人',
+      '1': '公司'
+    };
+
+    return function(id) {
+      return map[id] || id;
+    };
+  }
+
+  function agencyType() {
+    var map = {
+      '0': '独家',
+      '1': '非独家'
+    };
+
+    return function(id) {
+      return map[id] || id;
+    };
+  }
+
+  function applyRole() {
+    var map = {
+      '0': '经销商',
+      '1': '小店'
+    };
+
+    return function(id) {
+      return map[id] || id;
+    };
+  }
+
+  function payMode() {
+    var map = {
+      '0': '银行转账',
+      '1': '支付宝',
+      '2': '刷卡',
+      '3': '现金'
+    };
+
+    return function(id) {
+      return map[id] || id;
+    };
+  }
+
+  function payment() {
+    var map = {
+      '0': '定金',
+      '1': '全额'
+    };
+
+    return function(id) {
+      return map[id] || id;
+    };
+  }
+
+  function inviteStatus() {
+    var map = {
+      '0': '已邀约',
+      '1': '已到访'
+    };
+
+    return function(id) {
+      return map[id] || id;
+    };
   }
 })();
