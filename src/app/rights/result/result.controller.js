5f8b7b48ae16fddd2963fa44af2435c007cb49ea
Hom Yan <yanzhihong23@qq.com>
2015-12-10 02:37:09 +0800

    add rights apply info data format service

--- src/app/rights/result/result.controller.js
+++ src/app/rights/result/result.controller.js
@@ -6,7 +6,7 @@
     .controller('RightsResultController', RightsResultController);
 
   /** @ngInject */
-  function RightsResultController($log, ApiService, $state, $stateParams, localStorageService) {
+  function RightsResultController($log, ApiService, $state, $stateParams, localStorageService, FormatService) {
     var vm = this, id = $stateParams.id;
     vm.id = id;
 
@@ -19,68 +19,7 @@
     function getDetail() {
       ApiService.rightsApplyDetail({id: id}).success(function(data) {
         if(data.flag === 1) {
-          var obj = data.data.result;
-          vm.info = {
-            isTriggered: obj.whetherStartApply,
-            applyStatus: obj.applyStatus,
-            date: obj.createdDate,
-            companyName: obj.companyName,
-            realname: obj.applyName,
-            idNo: obj.personNum,
-            phone: obj.phone,
-            postAddr: obj.postAddr,
-            email: obj.mailAddr,
-            applyType: {
-              id: obj.applyType
-            },
-            applicantType: {
-              id: obj.applyPersonType
-            },
-            agencyType: {
-              id: obj.agencyType
-            },
-            payMode: {
-              id: obj.payWay
-            },
-            payType: {
-              id: obj.paymoneyType
-            },
-            area: {
-              province: {
-                id: obj.capitalId,
-                name: obj.capital
-              },
-              city: {
-                id: obj.cityId,
-                name: obj.city
-              },
-              district: {
-                id: obj.districtId,
-                name: obj.district
-              }
-            },
-            dealerCount: obj.dealershipNum,
-            storeCount: obj.storeNumm,
-            emergencyContact: obj.urgencyPerson,
-            emergencyPhone: obj.urgencyPhone,
-            contractAmount: obj.contractAmount,
-            paidAmount: obj.paidMoney,
-            payAmount: obj.needPaymoney,
-
-            bank: {
-              name: obj.payBank,
-              id: obj.payBankId
-            },
-            bankBranch: obj.paySubbranchBank,
-            bankAccount: obj.bankNumome,
-
-            alipayAccount: obj.payAlipayNum,
-
-            posNo: obj.possNum,
-
-            managerCheck: obj.managersCheck,
-            financeCheck: obj.financeCheck
-          }
+          vm.info = FormatService.applyInfoFormat(data.data.result);
 
           localStorageService.set('rightsApplyPreview', vm.info);
         } else {
---
+++ src/app/services/format.service.js
@@ -0,0 +1,80 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('crmApp')
+    .service('FormatService', FormatService);
+
+  /** @ngInject */
+  function FormatService() {
+    this.applyInfoFormat = function(obj) {
+      if(obj) {
+        return {
+          isTriggered: obj.whetherStartApply,
+          applyStatus: obj.applyStatus,
+          date: obj.createdDate,
+          companyName: obj.companyName,
+          realname: obj.applyName,
+          idNo: obj.personNum,
+          phone: obj.phone,
+          postAddr: obj.postAddr,
+          email: obj.mailAddr,
+          applyType: {
+            id: obj.applyType
+          },
+          applicantType: {
+            id: obj.applyPersonType
+          },
+          agencyType: {
+            id: obj.agencyType
+          },
+          payMode: {
+            id: obj.payWay
+          },
+          payType: {
+            id: obj.paymoneyType
+          },
+          area: {
+            province: {
+              id: obj.capitalId,
+              name: obj.capital
+            },
+            city: {
+              id: obj.cityId,
+              name: obj.city
+            },
+            district: {
+              id: obj.districtId,
+              name: obj.district
+            }
+          },
+          dealerCount: obj.dealershipNum,
+          storeCount: obj.storeNumm,
+          emergencyContact: obj.urgencyPerson,
+          emergencyPhone: obj.urgencyPhone,
+          contractAmount: obj.contractAmount,
+          paidAmount: obj.paidMoney,
+          payAmount: obj.needPaymoney,
+
+          bank: {
+            name: obj.payBankName,
+            id: obj.payBankCode
+          },
+          bankBranch: obj.paySubbranchBank,
+          bankAccount: obj.bankNumone,
+
+          alipayAccount: obj.payAlipayNum,
+
+          posNo: obj.possNum,
+
+          managerCheck: obj.managersCheck,
+          financeCheck: obj.financeCheck,
+          checkerId: obj.whoCheck
+        };
+      } else {
+        return {};
+      }
+    };
+
+  }
+})();
