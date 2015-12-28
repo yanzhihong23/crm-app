a4a80395ff91bf6a024edb197535143be127e90b
Hom Yan <yanzhihong23@qq.com>
2015-12-28 11:34:00 +0800

    build fix

--- gulp/build.js
+++ gulp/build.js
@@ -84,7 +84,7 @@ gulp.task('other', function () {
 
   return gulp.src([
     path.join(conf.paths.src, '/**/*'),
-    path.join('!' + conf.paths.src, '/**/*.{html,css,scss}')
+    path.join('!' + conf.paths.src + '/app', '/**/*.{html,css,js,scss}')
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
